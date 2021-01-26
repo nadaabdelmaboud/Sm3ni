57e8688d1da42a484c15232ef334cb8c6af897de
hagerali99 <hager.aismail@gmail.com>
2021-01-26 18:38:53 +0200

    beams added

---
+++ src/features/Beams.py
@@ -0,0 +1,48 @@
+from skimage.measure import label, regionprops
+from Blobs_VerticalLines import *
+
+
+def checkBeams(width, height, linesCount, maxSpace):
+    if width > 3*maxSpace and height > 2.5*maxSpace and linesCount > 1:
+        return True
+    return False
+
+
+def RemoveHorizontalAndDiagonalLines(img, originalImg, maxSpace, linesCount):
+    if(not checkBeams(img.shape[1], img.shape[0], linesCount, maxSpace)):
+        return img, 0, originalImg
+
+    removed = np.copy(img)
+    labelImg = label(img)
+    height, width = img.shape
+    regions = regionprops(labelImg)
+    diagonal = []
+    for r in regions:
+        minr, minc, maxr, maxc = r.bbox
+        aspectratio = (maxc-minc)/(maxr-minr)
+        if(aspectratio >= 1.5 and (maxc-minc) > .4*maxSpace):
+            diagonal.append([minr, minc, maxr, maxc])
+            removed[minr:maxr, minc:maxc] = 0
+            originalImg[minr:maxr, :] = 0
+    count = 0
+    if len(diagonal) > 0:
+        count = 1
+        minr, minc, maxr, maxc = diagonal[0]
+        col = (maxc+minc)/2
+        for i in range(1, len(diagonal)):
+            minr, minc, maxr, maxc = diagonal[i]
+            if(minc < col and maxc > col):
+                count += 1
+
+    return removed, count, originalImg
+
+
+def detectBeams(img, maxSpace):
+    img = np.array(img)
+    orig = np.copy(1-img)
+    img, peaksMids, maxProjection, width = removeSymbolVerticalLines(
+        img, maxSpace)
+    img = np.array(img)
+    removed, count, orig = RemoveHorizontalAndDiagonalLines(
+        1-img, orig, maxSpace, len(peaksMids))
+    return removed, count, 1-orig, peaksMids, maxProjection
